560be692012a7088f8871bdcd508f300457758cb
Ariana Gregg <ariana_gregg@mines.edu>
2026-04-29 17:33:38 -0600

    feat: add eden (switch emulator)

--- flake.nix
+++ flake.nix
@@ -5,24 +5,44 @@
 
   outputs = { self, nixpkgs }:
     let
-      version = "1.3.274";
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in
       rec {
-        packages.${system}.default = pkgs.appimageTools.wrapType2 {
-          name = "ryubing";
-          version = "${version}";
-          pname = "ryubing";
-          src = pkgs.fetchurl {
-            url = "https://git.ryujinx.app/Ryubing/Canary/releases/download/${version}/ryujinx-canary-${version}-x64.AppImage";
-            hash = "sha256-bCmqYv2tCnF9oVlnTulArfcPm2yrZntWMjJcz1pe96U=";
+        packages.${system} = {
+          ryubing = let
+            version = "1.3.274";
+          in pkgs.appimageTools.wrapType2 {
+            name = "ryubing";
+            version = "${version}";
+            pname = "ryubing";
+            src = pkgs.fetchurl {
+              url = "https://git.ryujinx.app/Ryubing/Canary/releases/download/${version}/ryujinx-canary-${version}-x64.AppImage";
+              hash = "sha256-bCmqYv2tCnF9oVlnTulArfcPm2yrZntWMjJcz1pe96U=";
+            };
+            extraPkgs = pkgs: with pkgs; [ icu ];
           };
-          extraPkgs = pkgs: with pkgs; [ icu ];
+          eden = let
+            version = "v0.2.0-rc2";
+          in pkgs.appimageTools.wrapType2 {
+            name = "eden";
+            version = "${version}";
+            pname = "eden";
+            src = pkgs.fetchurl {
+              url = "https://stable.eden-emu.dev/${version}/Eden-Linux-${version}-amd64-gcc-standard.AppImage";
+              hash = "sha256-1Pp6VInWYfr8f8ANuT1ZBxe61xCWcTq/mNH8T6JZJJc=";
+            };
+          };
         };
-        apps.${system}.default = {
-          type = "app";
-          program = "${packages.${system}.default}/bin/ryubing";
+        apps.${system} = {
+          ryubing = {
+            type = "app";
+            program = "${packages.${system}.ryubing}/bin/ryubing";
+          };
+          eden = {
+            type = "app";
+            program = "${packages.${system}.eden}/bin/eden";
+          };
         };
       };
 }
